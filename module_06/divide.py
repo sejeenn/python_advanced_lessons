78c4667120b83a3c8d16c44417883d125b3d2098
Eugene Vorontsov <sejenn@gmail.com>
2023-12-27 17:11:49 +0300

    271223w

--- module_06/divide.py
+++ module_06/divide.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from wtforms.validators import InputRequired
+import logging
 
 app = Flask(__name__)
+logger = logging.getLogger("divider")
 
 
 class DivideForm(FlaskForm):
@@ -16,17 +18,21 @@ def divide():
     form = DivideForm()
     if form.validate_on_submit():
         a, b = form.a.data, form.b.data
-        print('a, b: ', a, b)
+        logger.debug(f"форма валидна, а={a}, b={b}")
         return f"a / b = {a / b:.2f}"
+    logger.error(f"форма не валидна, error={form.errors}")
     return f"Неверный запрос", 400
 
 
 @app.errorhandler(ZeroDivisionError)
 def handle_exception(obj_err: ZeroDivisionError):
+    logger.exception("На ноль делить нелья!", exc_info=obj_err)
     return "На ноль делить нельзя", 400
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logger.info("Сервер деления чисел стартовал!")
     app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
 
--- module_06/input_password.py
+++ module_06/input_password.py
@@ -1,35 +1,45 @@
 import hashlib
 import getpass
+import logging
 # def create_password():
 #     password = input("Введите ваш пароль: ")
 #     hash_obj = hashlib.md5(password.encode())
 #     print(hash_obj.hexdigest())
 
+logger = logging.getLogger("password_checker")
+
 
 def input_and_check_password():
+    logger.debug("Начало input_and_check_password()")
+
     password: str = getpass.getpass()
     if not password:
-        print('Пароль не может быть пустым!')
+        logger.warning('Пароль не может быть пустым!')
         return False
     try:
         hasher = hashlib.md5()
         hasher.update(password.encode('latin-1'))
         if hasher.hexdigest() == "d8578edf8458ce06fbc5bb76a58c5ca4":
-            print('Пароль введен правильно!')
+            logger.info('Пароль введен правильно!')
             return True
     except ValueError:
         pass
-    print('Пароль введен неверно!')
+    logger.warning('Пароль введен неверно!')
     return False
 
 
 if __name__ == '__main__':
     # создание хешированного пароля
     # create_password()
+
+    logging.basicConfig(level=logging.DEBUG)
+    logger.info("Вы пытаетесь зарегистрироваться в системе.")
     count_number: int = 3
+    logger.info(f"У вас есть {count_number} попытки")
     while count_number > 0:
         if input_and_check_password():
             exit(0)
         count_number -= 1
+    logger.error("Пользователь трижды ввел неправильный пароль!")
     exit(1)
 
